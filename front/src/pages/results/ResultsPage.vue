381b6623e7e20dc0e0d5e636220de5cdb7fcedf9
Stephanie_marin <steph.marin.guz@gmail.com>
2022-05-21 13:45:59 +0200

    results page was added

---
+++ front/src/pages/results/ResultsPage.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="results">
+    <h1>Results</h1>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Results',
+  data() {
+    return {
+      info: {}
+    }
+  },
+
+}
+</script>
+
+<style scoped>
+h1 {
+  font-style: italic;
+}
+</style>
