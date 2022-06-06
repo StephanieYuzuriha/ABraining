bae470eb832958bbe178223944c5b2cd70b52574
Stephanie_marin <steph.marin.guz@gmail.com>
2022-06-06 15:01:42 +0200

    refact test, and change name repository

--- back/test/round/test_round_post.py
+++ back/test/round/test_round_post.py
@@ -5,14 +5,14 @@ from src.domain.round import RoundsRepository
 
 def test_save_a_round_round_by_post_method():
     round_repository = RoundsRepository(temp_file())
-    app = create_app(repositories={"round": round_repository})
+    app = create_app(repositories={"rounds": round_repository})
     client = app.test_client()
     
-    body =  {"game_numer" : "1",
+    body =  {
             "game_name" : "Match",
             "id_user" : "01",
-            "wrong_matches" : "4",
-            "date" : (2020, 12, 25)}
+            "wrong_matches" : "4"
+            }
     
     response = client.post("/api/results", json = body)
     
