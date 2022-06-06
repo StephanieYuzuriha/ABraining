b82636ce19a0d024b621975ecaa054544d8f7e3e
Stephanie_marin <steph.marin.guz@gmail.com>
2022-06-06 14:57:37 +0200

    repository "rounds" added

--- back/app.py
+++ back/app.py
@@ -1,4 +1,5 @@
 import sqlite3
+from src.domain.round import RoundsRepository
 from src.webserver import create_app
 from src.domain.info import InfoRepository
 from src.domain.asocia import AsociaRepository
@@ -9,6 +10,7 @@ database_path = "data/database.db"
 repositories = {
     "info": InfoRepository(database_path),
     "asocia": AsociaRepository(database_path),
+    "rounds" : RoundsRepository(database_path)
 }
 
 app = create_app(repositories)
