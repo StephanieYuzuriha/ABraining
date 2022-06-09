8b90f14dded94b1e20b4bf34af9f4b220d479c7a
Stephanie_marin <steph.marin.guz@gmail.com>
2022-06-09 12:09:54 +0200

    Endpoint GET results was made, and the table with the results

--- front/src/pages/results/ResultsPage.vue
+++ front/src/pages/results/ResultsPage.vue
@@ -1,18 +1,43 @@
 <template>
   <div class="results">
     <h1>Results</h1>
+    <table>
+      <tr>
+        <th>Game number</th>
+        <th>Mistakes</th>
+        <th>Date</th>
+      </tr>
+      <tr v-for="round of this.results" :key="round">
+        <td>{{round.game_numer}}</td>
+        <td>{{round.wrong_matches}}</td>
+        <td>{{round.date}}</td>
+      </tr>
+    </table>
   </div>
 </template>
 
 <script>
-
+import config from "../../config.js"
 export default {
   name: 'Results',
   data() {
     return {
-      info: {}
+      results: {}
     }
   },
+
+  mounted() {
+    this.loadData();
+
+  },
+
+  methods: {
+    async loadData(){
+      const response = await fetch(`${config.API_PATH}/results`);
+      this.results = await response.json();
+      console.log(this.results)
+    }
+  }
 
 }
 </script>
