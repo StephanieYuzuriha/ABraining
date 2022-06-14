ccceb1c077e9cb84c2ecbdfcab8f18d86a1b0ae9
Stephanie_marin <steph.marin.guz@gmail.com>
2022-06-14 19:34:08 +0200

    logIn and singUp links were added. Also useStorage

--- front/src/App.vue
+++ front/src/App.vue
@@ -2,7 +2,26 @@
   <div id="nav">
     <router-link to="/">Home</router-link> | 
     <router-link to="/match">Match</router-link> | 
-    <router-link to="/results">Results</router-link>
+    <router-link to="/results">Results</router-link> | 
+    <router-link to="/signUp">Sign Up</router-link>  | 
+    <router-link to="/logIn">Log In</router-link>
   </div>
   <router-view />
 </template>
+
+<script>
+import { useStorage } from "@vueuse/core";
+
+export default {
+  data() {
+    return {
+      localUser: useStorage("user", {}),
+    };
+  },
+  computed: {
+    loggedUser() {
+      return this.localUser.name;
+    },
+  },
+};
+</script>
