353ea3fdbacbeef69e9a280011192fa3ad9bdb3e
Stephanie_marin <steph.marin.guz@gmail.com>
2022-06-15 20:17:54 +0200

    results page added.

--- front/src/pages/results/ResultsPage.vue
+++ front/src/pages/results/ResultsPage.vue
@@ -1,28 +1,42 @@
 <template>
   <div class="results">
     <h1>Results</h1>
-    <table>
-      <tr>
-        <th>Game number</th>
-        <th>Mistakes</th>
-        <th>Date</th>
-      </tr>
-      <tr v-for="round of this.results" :key="round">
-        <td>{{round.game_numer}}</td>
-        <td>{{round.wrong_matches}}</td>
-        <td>{{round.date}}</td>
-      </tr>
-    </table>
+    <section v-if="!userIsNotLogged">
+      <table >
+        <tr>
+          <th>Game number</th>
+          <th>Mistakes</th>
+          <th>Date</th>
+        </tr>
+        <tr v-for="round of this.results" :key="round">
+          <td>{{round.game_name}}</td>
+          <td>{{round.wrong_matches}}</td>
+          <td>{{round.date}}</td>
+        </tr>
+      </table>
+    </section>
+    <section v-else>
+      You have to
+      <a href="/signUp" class="signUp">
+        Sign Up
+      </a>
+      <p>or</p>
+      <a href="/lonIn" class="logIn">
+        Log In
+      </a>
+      to see your results.
+    </section>
   </div>
 </template>
 
 <script>
-import config from "../../config.js"
+import { getResults } from "@/services/api.js"
 export default {
   name: 'Results',
   data() {
     return {
-      results: {}
+      results: {},
+      userIsNotLogged: false,
     }
   },
 
@@ -33,9 +47,10 @@ export default {
 
   methods: {
     async loadData(){
-      const response = await fetch(`${config.API_PATH}/results`);
-      this.results = await response.json();
-      console.log(this.results)
+      this.results = await getResults()
+      if (this.results === false) {
+        this.userIsNotLogged = true
+      }
     }
   }
 
