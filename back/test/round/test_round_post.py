6316c0c30fd70308bb2b271e157dcf9f219ecf63
Stephanie_marin <steph.marin.guz@gmail.com>
2022-06-17 08:40:36 +0200

    test remove

--- back/test/round/test_round_post.py
+++ back/test/round/test_round_post.py
@@ -17,17 +17,3 @@ def test_save_a_round_by_post_method():
     
     assert response.status_code == 200    
     
-def test_save_a_round_by_post_method_with_different_user_id_auth():
-        round_repository = RoundsRepository(temp_file())
-        app = create_app(repositories={"rounds": round_repository})
-        client = app.test_client()
-
-        body =  {
-                "game_name" : "Match",
-                "id_user" : "01",
-                "wrong_matches" : "4"
-                }
-
-        response = client.post("/api/results", json = body, headers={"Authorization": "3"})
-
-        assert response.status_code == 401   
